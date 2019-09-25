93dc8eb9c1d614df1ca94194a34bc82ce5eda3c6
NambiarAthira <55741228+NambiarAthira@users.noreply.github.com>
2019-09-25 08:50:34 +0530

    Add files via upload

---
+++ skeleton/skeleton.m
@@ -0,0 +1,13 @@
+%% Code for skeletonization upon a KINECT recorded image.
+% Author: Athira Nambiar
+% Date  : 13-12-2013
+
+%% 
+clc;
+close all;
+clear all;
+%%
+
+%Input the RGB video from Kinect recording
+
+%Input the depth video from Kinect recording
